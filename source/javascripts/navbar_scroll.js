465dd3a073ebf9dcedd33e530a00f626c5d075a4
Pierre Liduena <assurancesvelo@gmail.com>
2016-12-12 18:14:32 +0100

    all changes

--- source/javascripts/navbar_scroll.js
+++ source/javascripts/navbar_scroll.js
@@ -1,18 +1,22 @@
-// Navbar transition on scroll
+/*// Navbar transition on scroll
 
 $(function(){
   $(window).scroll(function(e){
     if ($(this).scrollTop() > 0) {
       $(".navbar").css({
-        "background": "rgba(0, 0, 0, 0.7)",
-        "box-shadow": "0 0 2px black"
+        "background": "white",
+        "border-bottom" : "1px solid #999999",
+        "navbar-color": "black"
+        // "box-shadow": "0 0 2px black"
       });
     }
     else {
       $(".navbar").css({
         "background": "transparent",
-         "box-shadow": "0 0 0px transparent"
+         "box-shadow": "0 0 0px transparent",
+         "border-bottom" : "transparent"
       });
     }
   });
 })
+*/
