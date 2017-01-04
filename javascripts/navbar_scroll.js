e760146795913c764eff0cc43dd26a83027e8c1c
assurancesvelo@gmail.com <assurancesvelo@gmail.com>
2017-01-04 11:41:09 +0100

    Automated commit at 2017-01-04 10:41:07 UTC by middleman-deploy 1.0.0

---
+++ javascripts/navbar_scroll.js
@@ -0,0 +1,24 @@
+/*// Navbar transition on scroll
+
+$(function(){
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 0) {
+      $(".navbar").css({
+        "background": "white",
+        "border-bottom" : "1px solid #999999",
+        "navbar-color": "black"
+        // "box-shadow": "0 0 2px black"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+         "box-shadow": "0 0 0px transparent",
+         "border-bottom" : "transparent"
+      });
+    }
+  });
+})
+*/
+
+;
